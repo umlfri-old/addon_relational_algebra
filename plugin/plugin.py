e1d0b63e49c95d5d2e9227a3fdc6281d34563cef
belas <belas.michal@gmail.com>
2012-02-15 14:17:07 +0000

    creating composite

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -4,7 +4,7 @@ from org.umlfri.api.mainLoops import GtkMainLoop
 from gtk.gdk import WindowTypeHint
 import MySQLdb
 from connect import Connection
-
+from operations import *
 
 
 def pluginMain(interface):
@@ -12,6 +12,7 @@ def pluginMain(interface):
     menu.add_submenu()
     submenu = menu.submenu
     submenu.add_menu_item('Pripoj', lambda x:menuConnect(), -1, 'Pripoj k databaze')
+    submenu.add_menu_item('Vykonaj',lambda z:execute(interface),-1,'Vykonaj')
     interface.transaction.autocommit = True
     interface.set_main_loop(GtkMainLoop())
 def menuConnect():
@@ -26,24 +27,70 @@ def menuConnect():
     c=b.get_object("button1")
     c.connect("clicked",lambda x:connect())
 
-
 def connect():
     #db = MySQLdb.connect(host=b.get_object("entry1").get_text(),user=b.get_object("entry3").get_text(),
     #passwd=b.get_object("entry4").get_text(),db=b.get_object("entry2").get_text())
     #db = MySQLdb.connect(host="localhost",user="belas",passwd="824510802",db="skuska")
     global w
     w.hide()
+    global a
     a=Connection()
     a.pripoj(1)
-    b=a.dajData('tabulka')
-    for row in b:
-        print row
     #cursor=db.cursor()
     #cursor.execute('SELECT * FROM tabulka')
     #for row in cursor:
     #   print row
+def execute(interface):
+    array=[]
+    a = interface.current_diagram.selected
+    tem=list(a)
+    if len(tem) == 1:
+        select=a.next()
+        create(select)
 
+            
 
+    else:
+        print "musis oznacit nejaky element"
+    #for element in a:
+     #   if element.object.type.name=="Selection":
+      #
+
+def create(self,trunk):
+    name=trunk.object.type.name
+    if name=="Union" :
+        pass
+    elif name=="Intersection":
+        pass
+    elif name=="Product":
+        pass
+    elif name=="Difference":
+        pass
+    elif name=="Division":
+        pass
+    elif name=="Selection":
+        object=Selection(select.object.values["column1"],select.object.values["condition"],select.object.values["column2"])
+        array.append(object)
+    elif name=="Projection":
+        pass
+    elif name=="Inner join":
+        pass
+    elif name=="Left outter join":
+        pass
+    elif name=="Right outter join":
+        pass
+    elif name=="Full outter join":
+        pass
+    cons=trunk.connections
+    tem1=list(cons)
+    i=0
+    while(i<len(tem1)):
+        con=cons.next()
+        object1=con.source()
+        object.set(object1)
+
+    create(object1)
+        
 
 
 
