174bc715c4cd535dcbdbc7908e152f293c7402d5
belas <belas.michal@gmail.com>
2012-02-17 18:53:30 +0000

    continue of composite structure

--- plugin/operations.py
+++ plugin/operations.py
@@ -1,18 +1,24 @@
-class Tabulka:
+class Table:
     def __init__(self,data,table):
         self.__database=data
         self.__table_name=table
 
     def execute(self):
-        return self.__database.dajData(self.__table_name)
-
+        return 1
+        #return self.__database.dajData(self.__table_name)
+    def __str__(self):
+        return "ja som table s menom" + self.__table_name
 class Projection:
     def __init__(self,data):
         self.__data=data
     def set(self,ancestor):
         self.__ancestor=ancestor
     def execute(self):
-        self.__ancestor.execute()
+        ret=self.__ancestor.execute()
+        return ret+1
+    def __str__(self):
+        ret=self.__ancestor.__str__()
+        return ret+"..."+"ja som projection "
 class Selection:
     def __init__(self,column,condition,data):
         self.__column=column
@@ -21,4 +27,25 @@ class Selection:
     def set(self,ancestor):
         self.__ancestor=ancestor
     def execute(self):
-        self.__ancestor.execute()
+        ret=self.__ancestor.execute()
+        return ret+2
+    def __str__(self):
+        ret=self.__ancestor.__str__()
+        return ret+"..."+"ja som select"
+class Union:
+    def __init__(self):
+        self.__ancestor_left=None
+        self.__ancestor_right=None
+    def set(self,ancestor):
+        if(self.__ancestor_left==None):
+            self.__ancestor_left=ancestor
+        else:
+            self.__ancestor_right=ancestor
+    def execute(self):
+        ret=self.__ancestor_left.execute()
+        ret1=self.__ancestor_right.execute()
+        return ret*ret1
+    def __str__(self):
+        ret=self.__ancestor_left.__str__()
+        ret1=self.__ancestor_right.__str__()
+        return ret+ret1+"..."+"ja som union"
