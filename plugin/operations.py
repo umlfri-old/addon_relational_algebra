eaabe32003c8e919917fb54329c5d7bcbf7b7456
belas <belas.michal@gmail.com>
2012-02-23 09:24:44 +0000

    added execute function for Difference,Division,Intersection,Union,Product,Selection

--- plugin/operations.py
+++ plugin/operations.py
@@ -63,6 +63,41 @@ class Selection:
         self.__ancestor=ancestor
     def execute(self):
         ret=self.__ancestor.execute()
+        columns=ret[0]
+        try:
+            index=columns.index(self.__column)
+        except ValueError:
+            return None
+        table=[]
+        for i in range(0,len(ret)):
+            new=[]
+            for y in range(0,len(columns)):
+                try:
+                    if(ret[i][index]):
+                        pass
+                except ValueError:
+                    pass
+            table.append(new)
+class Product:
+    def __init__(self):
+        self.__ancestor_left=None
+        self.__ancestor_right=None
+    def set(self,ancestor):
+        if(self.__ancestor_left==None):
+            self.__ancestor_left=ancestor
+        else:
+            self.__ancestor_right=ancestor
+    def execute(self):
+        ret=self.__ancestor_left.execute()
+        ret1=self.__ancestor_right.execute()
+        table=[]
+        columns=ret[0]+ret1[0]
+        table.append(columns)
+        for i in range(1,len(ret)):
+            for y in range(1,len(ret1)):
+                new=ret[i]+ret1[y]
+                table.append(new)
+        return table
 class Union:
     def __init__(self):
         self.__ancestor_left=None
@@ -75,4 +110,98 @@ class Union:
     def execute(self):
         ret=self.__ancestor_left.execute()
         ret1=self.__ancestor_right.execute()
-        return ret + ret1
+        columns=ret[0]
+        columns1=ret1[0]
+        table=[]
+        if columns==columns1:
+            table.append(columns)
+            for i in range(1,len(ret)):
+                table.append(ret[i])
+            for i in range(1,len(ret1)):
+                table.append(ret1[i])
+        else:
+            print "union incompatible error"
+            return None
+        return table
+class Intersection:
+    def __init__(self):
+        self.__ancestor_left=None
+        self.__ancestor_right=None
+    def set(self,ancestor):
+        if(self.__ancestor_left==None):
+            self.__ancestor_left=ancestor
+        else:
+            self.__ancestor_right=ancestor
+    def execute(self):
+        ret=self.__ancestor_left.execute()
+        ret1=self.__ancestor_right.execute()
+        columns=ret[0]
+        columns1=ret1[0]
+        table=[]
+        if columns==columns1:
+           table.append(columns)
+           for i in range(1,len(ret)):
+               try:
+                   ret1.index(ret[i])
+                   table.append(ret[i])
+               except ValueError:
+                   pass
+        else:
+            print "intersection incompatible error"
+            return None
+        return table
+class Division:
+    def __init__(self):
+        self.__ancestor_left=None
+        self.__ancestor_right=None
+    def set(self,ancestor):
+        if(self.__ancestor_left==None):
+            self.__ancestor_left=ancestor
+        else:
+            self.__ancestor_right=ancestor
+    def execute(self):
+        ret=self.__ancestor_left.execute()
+        ret1=self.__ancestor_right.execute()
+        columns=ret[0]
+        columns1=ret1[0]
+        print columns
+        print columns1
+        table=[]
+        indexes=[]
+        for i in range(0,len(columns1)):
+            try:
+                indexes.append(columns.index(columns1[i]))
+                columns.remove(columns1[i])
+            except ValueError:
+                print "division error"
+                return None
+        table.append(columns)
+        return table
+class Difference:
+    def __init__(self):
+        self.__ancestor_left=None
+        self.__ancestor_right=None
+    def set(self,ancestor):
+        if(self.__ancestor_left==None):
+            self.__ancestor_left=ancestor
+        else:
+            self.__ancestor_right=ancestor
+    def execute(self):
+        ret=self.__ancestor_left.execute()
+        ret1=self.__ancestor_right.execute()
+        columns=ret[0]
+        columns1=ret1[0]
+        print columns
+        print columns1
+        table=[]
+        table.append(columns)
+        if columns==columns1:
+            for i in range(1,len(ret)):
+                try:
+                    ret1.index(ret[i])
+                except ValueError:
+                    table.append(ret[i])
+        else:
+            print "difference error"
+            return None
+        return table
